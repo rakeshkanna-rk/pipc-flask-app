6619603b0a64a0d04fe166b51b27a72f4cf61a74
rakeshkanna-rk <rakeshkanna0108@gmail.com>
2024-10-08 23:18:26 +0530

    updated docs

--- pipc_flask_app/main.py
+++ pipc_flask_app/main.py
@@ -7,12 +7,15 @@ title = f"{BLUE}PIPCREATOR FLASK APP{RESET}"
 
 @click.group()
 def cli():
-    print(title)
+    print(f"\n{title}")
     pass
 
-@click.command()
-@click.argument('directory')
-def create_flask_app(directory):
+@click.command(help="Create Flask App")
+@click.argument('directory', required=False)
+def create(directory):
+    while not directory:
+        if not directory:
+            directory = input("Enter location / project name: ")
     create_flask(directory)
 
-cli.add_command(create_flask_app)
+cli.add_command(create)
